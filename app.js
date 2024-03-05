94165f09637348a73e3c23fe38338c08cdbf9a08
abduxanon <abduxanonaxmadaliyev@gmail.com>
2024-03-05 23:26:51 +0500

    salom

---
+++ app.js
@@ -0,0 +1,48 @@
+// 10 misol
+
+// let tomon1 = +prompt("tomon1");
+// let tomon2 = +prompt("tomon2");
+// function formate(tomon1, tomon2) {
+//   let result = tomon1 + tomon2 - 1;
+//   return result;
+// }
+// console.log(formate(tomon1,tomon2));
+
+//11 misol
+
+// let son1 = +prompt("son1");
+// let son2 = +prompt("son2");
+// function formate(s1, s2) {
+//   let result = s1 % s2;
+//   return result;
+// }
+// console.log(formate(son1, son2));
+
+//13 misol
+
+// let str1 = prompt("1 str kriting");
+// let str2 = prompt("2 str nikiriting");
+// function string(s1, s2) {
+//   let result = s1 + s2;
+//   return result;
+// }
+// console.log(string(str1, str2));
+
+//14 misol
+
+// let kara1 = +prompt("1 soni kriting");
+// let kara2 = +prompt("2 soni kriting");
+// function example(k1, k2) {
+//   let result = `${k1} * ${k2} = ${k1 * k2}`;
+//   return result;
+// }
+// console.log(example(kara1, kara2));
+
+//15 misol
+
+// let n = +prompt("son kriting");
+// function example(n1) {
+//   let result = (n1 - 2) * 180;
+//   return result;
+// }
+// console.log(example(n));
